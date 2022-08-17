e27750c6acd14ab46d47a53110c85ff91ba946ec
Sharonda Daniels <srdanie2@ncsu.edu>
2022-08-17 02:57:20 -0400

    Added app icons

--- Prework/SceneDelegate.swift
+++ Prework/SceneDelegate.swift
@@ -44,7 +44,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        window?.endEditing(true)
 
     }
     
